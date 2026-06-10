8da6400d3068546adeef219a2706af434ab13767
AcuacheY <v.acuache15@gmail.com>
2026-06-10 00:57:01 -0500

    Remove unused GameDetail and GamePlayer components; add new Library component for game page

--- app/biblioteca/[id]/jugar/page.tsx
+++
@@ -1,6 +0,0 @@
-import GamePlayer from "@/components/GamePlayer";
-
-export default async function Page(props: PageProps<"/biblioteca/[id]/jugar">) {
-  const { id } = await props.params;
-  return <GamePlayer id={id} />;
-}
--- app/biblioteca/[id]/page.tsx
+++
@@ -1,6 +0,0 @@
-import GameDetail from "@/components/GameDetail";
-
-export default async function Page(props: PageProps<"/biblioteca/[id]">) {
-  const { id } = await props.params;
-  return <GameDetail id={id} />;
-}
